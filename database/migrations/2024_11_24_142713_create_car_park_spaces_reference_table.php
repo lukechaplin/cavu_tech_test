f6bd807292d4b8089513aa1a7b6b65df08ae9cb0
lukechaplin <lukechaplin@gmail.com>
2024-11-24 14:34:09 +0000

    added car park spaces reference table migration file

---
+++ database/migrations/2024_11_24_142713_create_car_park_spaces_reference_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+ public function up(): void
+    {
+        Schema::create('car_park_spaces_reference', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        // Adds a database trigger to limit the number of rows to 10
+        DB::unprepared('
+            CREATE TRIGGER limit_car_park_spaces
+            BEFORE INSERT ON car_park_spaces
+            FOR EACH ROW
+            BEGIN
+                IF (SELECT COUNT(*) FROM car_park_spaces) >= 10 THEN
+                    SIGNAL SQLSTATE "45000"
+                    SET MESSAGE_TEXT = "Cannot insert more than 10 car park spaces";
+                END IF;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+         // Drops the trigger first
+        DB::unprepared('DROP TRIGGER IF EXISTS limit_car_park_spaces');
+
+        Schema::dropIfExists('car_park_spaces_reference');
+    }
+};
